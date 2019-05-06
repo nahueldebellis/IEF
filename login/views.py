735cbe88b4e6644a59926defc3053e0d2e3d6d9d
nahueldebellis <debellisnahuel@gmail.com>
2019-05-05 23:53:05 -0300

    add cookies to persist session in user login

--- login/views.py
+++ login/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render, redirect
-from django.core.mail import EmailMessage
 from django.http import HttpResponse
 from django.views import View
 from .models import Users
-from .forms import RegisterForm, LoginForm
+
+import hashlib
 import json
 
 # Create your views here.
 class Register(View):
     def get(self, request):
-        form = RegisterForm()
         return render(request, 'login/Index_Register.html')
 
     def post(self, request):
         user_exist = Users.objects.get(email=request.POST['email'])
         if not user_exist:
-            user = Users(firstname=request.POST['name'], lastname=request.POST['lname'], 
-            dni=request.POST['dni'], email=request.POST['email'], bio=request.POST['bio'], 
-            password=request.POST['password'], sex=request.POST['sexo'],
-            phone=request.POST['tel'], cuil=request.POST['cuil'], address=request.POST['dir'], 
-            country=request.POST['country'], province=request.POST['prov'], location=request.POST['loc'], 
-            cp=request.POST['cp'], interest=request.POST['interest'])
+            user = Users(
+                firstname=request.POST['name'], lastname=request.POST['lname'], 
+                dni=request.POST['dni'], email=request.POST['email'], bio=request.POST['bio'], 
+                password=request.POST['password'], sex=request.POST['sexo'],
+                phone=request.POST['tel'], cuil=request.POST['cuil'], address=request.POST['dir'], 
+                country=request.POST['country'], province=request.POST['prov'], location=request.POST['loc'], 
+                cp=request.POST['cp'], interest=request.POST['interest']
+            )
 
             user.save()
 
@@ -37,12 +38,19 @@ class Confirmation(View):
 
 class Login(View):
     def get(self, request):
-        form = LoginForm()
-        return render(request, 'login/Index_Login.html')
+        try:
+            if(request.session.get('user_session', 'none') != 'none'):
+                return HttpResponse('Perfil')
+            else:
+                return render(request, 'login/Index_Login.html', {'action': '/welcome/login/'})
+        except:
+            return HttpResponse(status=500)
 
     def post(self, request):
         try:
             user = Users.objects.get(email=request.POST['email'], password=request.POST['password'])
-            return HttpResponse('Perfil')
+            hash_user = hashlib.new("sha1", b"%s" % (user.id))
+            request.session['user_session'] = hash_user
+            return render(request, 'Perfil')
         except:
-            return render(request, 'login/Index_Login.html', {'error': 'Email o contraseña incorrectos'})
+            return render(request, 'Error, user not register')
